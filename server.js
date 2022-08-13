bdd6f2d004db6d693304a33a9ae514c18f67a31e
Bebby Ilham Akbar Aresta <47028105+bebbyilham@users.noreply.github.com>
2022-08-14 01:01:04 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const socket = require("socket.io");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+const port = process.env.PORT;
+
+server.listen(port, function () {
+  console.log("psc-websocket is running");
+});
+io.on("connection", function (socket) {
+  socket.on("incident_notification", function (data) {
+    io.sockets.emit("incident_notification", {
+      lokasi: data.lokasi,
+      deskripsi: data.deskripsi,
+      nohp: data.no_hp,
+      namapelapor: data.nama_pelapor,
+      latkejadian: data.lat_kejadian,
+      lngkejadian: data.lng_kejadian,
+    });
+  });
+});
